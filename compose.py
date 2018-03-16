1fcc81ccff378d6c34786b82bc31eb5710f16654
ian <zyktnn@gmail.com>
2018-03-16 14:10:48 +0300

    Cleaning up

--- compose.py
+++ compose.py
@@ -1,4 +1,4 @@
-class Compose:
+class compose:
     def __init__(self, func):
         self.func = func
 
@@ -16,22 +16,7 @@ class Compose:
             self.func = self.func(other)
         return self
 
-compose = Compose
-
 
 def _compose_helper(f, g):
     return lambda *args, **kwargs: f(g(*args, **kwargs))
-
-
-@compose
-def add1(x): return x + 1
-@compose
-def mul10(x): return x * 10
-
-add1mul10 = add1 >> mul10
-
-
-print(add1mul10(1))
-
-print(((lambda x: x / 2) >> add1)(10))
 
